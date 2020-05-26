2228dce48e91cc17097e84bcf8b6f35cd7948195
chlrhdms <silver_xix@naver.com>
2020-05-26 23:45:13 +0900

    게임 추가 ts

--- app.py
+++ app.py
@@ -1,26 +1,66 @@
 from flask import Flask, request, render_template, redirect, url_for
+
+import game
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def indaex():
     return '메인페이지'
 
-@app.route('/move/<name>')
-def input_name(name):
-    if name == 'naver':
-        return redirect(url_for('naver'))
-    elif name == 'daum':
-        return redirect(url_for('daum'))
+@app.route('/hello/<name>')
+def hellovar(name):
+    character = game.set_charact(name)
+    return render_template('gamestart.html', data=character)
+
+@app.route('/gamestart')
+def gamestart():
+    with open("static/save.txt", "r", encoding='utf-8') as f:
+        date = f.read()
+        character = json.loads(date)
+        print(character['items'])
+    return "{}이 {} 아이템을 사용했습니다".format(character['name'], character["items"][0])
+
+@app.route('/input/<int:num>')
+def input_num(num):
+    if num == 1:
+        with open("static/save.txt", "r", encoding='utf-8') as f:
+            date = f.read()
+            character = json.loads(date)
+            print(character['items'])
+        return "{}이 {} 아이템을 사용했습니다".format(character['name'], character["items"][0])
+    elif num == 2:
+        return '도망갔다'
+    elif num == 3:
+        return '퉁퉁이'
+    else: 
+        return "없어요"
+    # return "hello {}".format(name)
+
+@app.route('/form')
+def form():
+    return render_template('test.html')
+
+@app.route('/method', methods=['GET', 'POST'])
+def method():
+    if request.method == 'GET':
+        return 'GET 으로 전송이다.'
     else:
-        return "없는 사이트"
+        num = request.form['num']
+        name = request.form['name']
+        print(num, name)
+        with open("static/save.txt", "w", encoding='utf-8') as f:
+            f.write("%s,%s" % (num, name))
+        return 'POST 이다. 학번: {} 이름:{}'.format(num,name)
 
-@app.route('/naver')
-def naver():
-    return render_template("naver.html")
+@app.route('/getinfo') 
+def getinfo():
+    # 파일 입력
+    with open("static/save.txt", "r", encoding='utf-8') as file:
+        student = file.read().split(',') # 쉽표로 잘라서 student 에 배열로 저장
+    return '번호 : {}, 이름 : {}'.format(student[0], student[1])
 
-@app.route('/daum')
-def daum():
-    return redirect("https://www.daum.net/")
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ game.py
@@ -0,0 +1,22 @@
+import json
+def set_charact(name):
+
+
+    character = {
+        "name": name,
+        "level": 1,
+        "hp": 100,
+        "items": ["주먹", "발차기", "겁주기"],
+        "skill": ["방패", "순발력", "아기호랑이"]
+    }
+    with open("static/save.txt", "w", encoding="utf-8") as f:
+        json.dump(character, f, ensure_ascii = False, indent=4)
+    # print("{0}님의 모험을 시작합니다. (HP {1})".format(character['name'], character["hp"]))
+    return character
+
+def save_game(filename, charact):
+    f = open("save.txt", "w", encoding="utf-8")
+    for key in charact:
+        print("%s:%s"  % (key, charact[key]))
+        f.write("%s:%s\n" % (key, charact[key]))
+    f.close()
