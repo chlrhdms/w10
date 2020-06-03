014ef962156ef5ee966cf212b7f768806ad96e76
chlrhdms <silver_xix@naver.com>
2020-06-03 20:45:04 +0900

    sqlite db 소스 적용

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for
 
 import game
 import json
+import dbdb
 
 app = Flask(__name__)
 
@@ -50,16 +51,15 @@ def method():
         num = request.form['num']
         name = request.form['name']
         print(num, name)
-        with open("static/save.txt", "w", encoding='utf-8') as f:
-            f.write("%s,%s" % (num, name))
+        dbdb.insert_data(num, name)
         return 'POST 이다. 학번: {} 이름:{}'.format(num,name)
 
 @app.route('/getinfo') 
 def getinfo():
-    # 파일 입력
-    with open("static/save.txt", "r", encoding='utf-8') as file:
-        student = file.read().split(',') # 쉽표로 잘라서 student 에 배열로 저장
-    return '번호 : {}, 이름 : {}'.format(student[0], student[1])
+    ret = dbdb.select_all()
+    print(ret[3])
+    return render_template('getinfo.html', data=ret)
+    # return '번호 : {}, 이름 : {}'.format(ret[0], ret[1])
 
 
 if __name__ == '__main__':
