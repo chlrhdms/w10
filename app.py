2ab03ba17856fa0c97eff1244bc73262e76744ef
chlrhdms <silver_xix@naver.com>
2020-06-18 21:33:59 +0900

    12주차 과제

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 
 import game
 import json
@@ -10,56 +10,21 @@ app = Flask(__name__)
 def indaex():
     return '메인페이지'
 
-@app.route('/hello/<name>')
-def hellovar(name):
-    character = game.set_charact(name)
-    return render_template('gamestart.html', data=character)
-
-@app.route('/gamestart')
-def gamestart():
-    with open("static/save.txt", "r", encoding='utf-8') as f:
-        date = f.read()
-        character = json.loads(date)
-        print(character['items'])
-    return "{}이 {} 아이템을 사용했습니다".format(character['name'], character["items"][0])
-
-@app.route('/input/<int:num>')
-def input_num(num):
-    if num == 1:
-        with open("static/save.txt", "r", encoding='utf-8') as f:
-            date = f.read()
-            character = json.loads(date)
-            print(character['items'])
-        return "{}이 {} 아이템을 사용했습니다".format(character['name'], character["items"][0])
-    elif num == 2:
-        return '도망갔다'
-    elif num == 3:
-        return '퉁퉁이'
-    else: 
-        return "없어요"
-    # return "hello {}".format(name)
-
-@app.route('/form')
-def form():
-    return render_template('test.html')
-
-@app.route('/method', methods=['GET', 'POST'])
-def method():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'GET':
-        return 'GET 으로 전송이다.'
+        return render_template('login.html')
     else:
-        num = request.form['num']
-        name = request.form['name']
-        print(num, name)
-        dbdb.insert_data(num, name)
-        return 'POST 이다. 학번: {} 이름:{}'.format(num,name)
-
-@app.route('/getinfo') 
-def getinfo():
-    ret = dbdb.select_all()
-    print(ret[3])
-    return render_template('getinfo.html', data=ret)
-    # return '번호 : {}, 이름 : {}'.format(ret[0], ret[1])
+        id = request.form['id']
+        pw = request.form['pw']
+        print (id,type(id))
+        print (pw,type(pw))
+        # id와 pw가 db 값이랑 비교 해서 맞으면 맞다 틀리면 틀리다
+        ret = dbdb.select_user(id, pw)
+        if ret != None:
+            return "안녕하세요~ {} 님" .format(id)
+        else:
+            return "아이디 또는 패스워드를 확인 하세요."
 
 
 if __name__ == '__main__':
--- dbdb.py
+++ dbdb.py
@@ -23,38 +23,25 @@ def create_table():
     finally:
         db.close()
 
-def insert_data(num, name):
+def insert_user(id, pw):
     try:
         db = dbcon()
         c = db.cursor()
-        setdata = (num, name)
-        c.execute("INSERT INTO student VALUES (?, ?)", setdata)
+        setdata = (id, pw)
+        c.execute("INSERT INTO users VALUES (?, ?)", setdata)
         db.commit()
     except Exception as e:
         print('db error:', e)
     finally:
         db.close()
 
-def select_all():
-    ret = list()
-    try:
-        db = dbcon()
-        c = db.cursor()
-        c.execute('SELECT * FROM student')
-        ret = c.fetchall()
-    except Exception as e:
-        print('db error:', e)
-    finally:
-        db.close()
-    return ret
-
-def select_num(num):
+def select_user(id, pw):
     ret = ()
     try:
         db = dbcon()
         c = db.cursor()
-        setdata = (num,)
-        c.execute('SELECT * FROM student WHERE num = ?', setdata)
+        setdata = (id, pw)
+        c.execute('SELECT * FROM users WHERE id = ? AND pw = ?', setdata)
         ret = c.fetchone()
     except Exception as e:
         print('db error:', e)
@@ -63,7 +50,9 @@ def select_num(num):
     return ret
 
 # create_table()
+# insert_user('abc', '1234', '에비시')
 # insert_data('20201236', '디비')
 # ret = select_all()
 # ret = select_num('20201236')
+# ret = select_user('abc', '1234')
 # print(ret)
